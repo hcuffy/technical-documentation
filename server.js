6e0aede9cf0bc22c167f57e9df5e845b5d6ce3d5
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-23 17:21:58 +0200

    Update folder structure.

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const port = process.env.PORT || 3000;
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-console.log('Express server has started on', port);
-
+	console.log('Express server has started on', port);
 });
